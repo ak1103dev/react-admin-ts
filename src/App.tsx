f0fb6e33728c457a3d92c7b950585d6f042a44f8
ak1103dev <ak.apichan1103@gmail.com>
2021-08-09 12:38:51 +0700

    :construction: init antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import 'antd/dist/antd.less'
+import { DatePicker, Button } from 'antd'
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DatePicker />
+      <div>
+        <Button type="primary">Hello</Button>
+      </div>
     </div>
   )
 }
